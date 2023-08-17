b805d984e8eab1a1648b887bbdde8188e3cecc2d
wongluyee <wongluyee1015@hotmail.com>
2023-08-17 16:58:53 +0900

    list and update

--- src/jp/co/f1/study/jdbc/List.java
+++ src/jp/co/f1/study/jdbc/List.java
@@ -1,9 +1,56 @@
 package jp.co.f1.study.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class List {
+	public static String RDB_DRIVE = "com.mysql.jdbc.Driver";
+	public static String URL = "jdbc:mysql://localhost/mybookdb";
+	public static String USER = "root";
+	public static String PASSWORD = "password";
 
 	public static void main(String[] args) {
+		Connection con = null;
+		Statement smt = null;
 
+		try {
+			// ドライバを読み込む
+			Class.forName(RDB_DRIVE);
+			// データベースへ接続する
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			// ステートメント作成する
+			smt = con.createStatement();
+			String sql = "SELECT * FROM bookinfo";
+			ResultSet rs = smt.executeQuery(sql);
+
+			// データを表示する
+			while (rs.next()) {
+				System.out.println("ISBN: " + rs.getString("isbn") + " Title: " + rs.getString("title") + " Price: "
+						+ rs.getString("price"));
+			}
+
+		} catch (Exception e) {
+			System.out.println("エラー：" + e);
+		} finally {
+			try {
+				if (smt != null) {
+					System.out.println("Statementクローズします。");
+					smt.close();
+				}
+
+				if (con != null) {
+					System.out.println("DB接続をクローズします。");
+					con.close();
+				}
+
+			} catch (SQLException ignore) {
+				// 例外処理
+			}
+		}
 	}
 
 }
--- src/jp/co/f1/study/jdbc/Update.java
+++ src/jp/co/f1/study/jdbc/Update.java
@@ -1,9 +1,61 @@
 package jp.co.f1.study.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Update {
+	public static String RDB_DRIVE = "com.mysql.jdbc.Driver";
+	public static String URL = "jdbc:mysql://localhost/mybookdb";
+	public static String USER = "root";
+	public static String PASSWORD = "password";
 
 	public static void main(String[] args) {
+		Connection con = null;
+		Statement smt = null;
 
+		try {
+			// ドライバを読み込む
+			Class.forName(RDB_DRIVE);
+			// データベースへ接続する
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			// ステートメント作成する
+			smt = con.createStatement();
+			// 更新したいデータを特定する
+			String sql = "UPDATE bookinfo SET title = 'C' WHERE title = 'C++'";
+			// データを更新する
+			int rowsCount = smt.executeUpdate(sql);
+			System.out.println(rowsCount + " レコード更新しました。");
+
+			// すべてのデータを表示する
+			String listSql = "SELECT * FROM bookinfo";
+			ResultSet rs = smt.executeQuery(listSql);
+
+			while (rs.next()) {
+				System.out.println("ISBN: " + rs.getString("isbn") + " Title: " + rs.getString("title") + " Price: "
+						+ rs.getString("price"));
+			}
+
+		} catch (Exception e) {
+			System.out.println("エラー：" + e);
+		} finally {
+			try {
+				if (smt != null) {
+					System.out.println("Statementクローズします。");
+					smt.close();
+				}
+
+				if (con != null) {
+					System.out.println("DB接続をクローズします。");
+					con.close();
+				}
+
+			} catch (SQLException ignore) {
+				// 例外処理
+			}
+		}
 	}
 
 }
